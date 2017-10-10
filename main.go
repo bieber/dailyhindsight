19a39a231e6cd6034f0ef63a9d0672ab83de9a8c
Robert Bieber <robby@bieberphoto.com>
2017-10-10 00:08:30 -0700

    Initial Commit
    Added a skeleton for configuration, and also curated data sources.

---
+++ main.go
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2017, Robert Bieber
+ *
+ * This file is part of dailyhindsight.
+ *
+ * dailyhindsight is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dailyhindsight is distributed in the hope that it will be useful,
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dailyhindsight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"fmt"
+	"github.com/bieber/conflag"
+	"golang.org/x/crypto/ssh/terminal"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Help     bool
+	Port     int
+	APIKey   string
+	TempFile string
+	LogFile  string
+}
+
+func main() {
+	config, parser := getConfig()
+	_, err := parser.Read()
+	if err != nil || config.Help {
+		exitCode := 0
+
+		if err != nil {
+			log.Println(err)
+			exitCode = 1
+		}
+
+		if width, _, err := terminal.GetSize(0); err == nil {
+			fmt.Println(parser.Usage(uint(width)))
+		}
+		os.Exit(exitCode)
+	}
+}
+
+func getConfig() (*Config, *conflag.Config) {
+	config := &Config{
+		Port:     8080,
+		TempFile: "cache.txt",
+	}
+
+	parser, err := conflag.New(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	parser.ProgramName("dailyhindsight")
+	parser.ProgramDescription("HTTP server for Daily Hindsight")
+	parser.ConfigFileLongFlag("config")
+
+	parser.Field("Help").
+		ShortFlag('h').
+		Description("Print usage text and exit.")
+
+	parser.Field("Port").
+		ShortFlag('p').
+		Description("Port to serve HTTP traffic on.")
+
+	parser.Field("APIKey").
+		ShortFlag('k').
+		Required().
+		Description("API key for Quandl.")
+
+	parser.Field("TempFile").
+		ShortFlag('t').
+		Description("File to cache results for day in case server goes down.")
+
+	parser.Field("LogFile").
+		ShortFlag('l').
+		Description("Optional log output file (logs go to stderr by default)")
+
+	return config, parser
+}
