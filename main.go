e31505152bf85860bd1db231c86b08869c4f55b9
Robert Bieber <robby@bieberphoto.com>
2017-10-25 01:39:43 -0700

    Logging Server Start Failures
    Logging the error returned if ListenAndServe fails.

--- main.go
+++ main.go
@@ -108,7 +108,7 @@ func main() {
 		Middleware(IndexHandler(*config, &selection, &selectionLock)),
 	)
 	http.Handle("/favicon.ico", Middleware(FaviconHandler()))
-	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil))
 }
 
 func getConfig() (*Config, *conflag.Config) {
